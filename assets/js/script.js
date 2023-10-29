1ac4623bb76fb77c024b1ae141c26a0e64a12c34
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2023-10-29 15:13:37 -0400

    Adding the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var timeEl = document.querySelector(".time");
+
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 75;
+
+  function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time Remaining:" + secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+  setTime();
 //Header Items
 /// View HighScores link
 //// Display top 10 scores
