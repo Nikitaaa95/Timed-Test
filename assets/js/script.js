c237518a779f31a777040b61bf9a4577a22e2a36
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2023-10-29 14:43:11 -0400

    Adding basic code pages and a todo list

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Header Items
+/// View HighScores link
+//// Display top 10 scores
+/// See the timer countdown
+
+//Body Items
+///Include starter language
+////Add starter button - Prompt questions
+///Correct/Incorrect Indicator
+///Final Score - Include initials
